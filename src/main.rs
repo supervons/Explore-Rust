d905c0bf8e9c811a82800d954fa2762aa2c0aa6f
supervons <supervons@sina.com>
2023-04-23 15:16:13 +0800

    [feature] Say hello demo.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn greet_world() {
+    let southern_germany: &str = "Grüß Gott!";
+    let chinese: &str = "世界，你好！";
+    let english: &str = "World，hello";
+    let regions = [southern_germany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", region);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
