97af0a424a8fdf45af942ea153472e7bdfc1de8c
supervons <supervons@sina.com>
2023-04-25 22:49:43 +0800

    Import num lib.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use num::complex::Complex;
 fn greet_world() {
     let result = add(18, 19);
     println!("{}", result);
@@ -8,5 +9,9 @@ fn add(i: i32, j: i32) -> i32 {
 }
 
 fn main() {
+    let a = Complex { re: 2.1, im: -1.2 };
+    let b = Complex::new(11.1, 22.2);
+    let result = a + b;
+    println!("{} + {}i", result.re, result.im);
     greet_world();
 }
