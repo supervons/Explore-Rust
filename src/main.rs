24193994d2d84adfcc34c221252cacf71447c383
supervons <supervons@sina.com>
2023-04-26 23:55:40 +0800

    Add chars.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use num::complex::Complex;
 fn greet_world() {
     let result = add(18, 19);
     println!("{}", result);
@@ -9,9 +8,8 @@ fn add(i: i32, j: i32) -> i32 {
 }
 
 fn main() {
-    let a = Complex { re: 2.1, im: -1.2 };
-    let b = Complex::new(11.1, 22.2);
-    let result = a + b;
-    println!("{} + {}i", result.re, result.im);
+    let c = 'z';
+    let heart_eyed_cat = '😻';
+    println!("{} + {}", c, heart_eyed_cat);
     greet_world();
 }
